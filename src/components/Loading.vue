b0d0af9261ce84bdd54c9b3b7205c364dbe31e3d
Sabrina Luo <luohaojing@gmail.com>
2016-07-21 14:18:48 +0800

    add component loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loading"
+       v-show="isLoading">
+    <p class="emoji">>_<</p>
+    <p>Sorry, API真是非常！非常！非常的慢！！！多等几十秒好伐啦~ :P</p>
+    <p>loading...</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'loading',
+    props: {
+      isLoading: false
+    }
+  };
+</script>
+
+<style>
+  .loading {
+    width: 100%;
+    text-align: center;
+    margin: 50px 0;
+    color: #999;
+  }
+
+  .emoji{
+    font-size: 50px;
+    font-weight: bold;
+  }
+</style>
